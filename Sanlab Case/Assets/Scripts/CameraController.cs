256302ecc67c54c0d2534774a59de516b23d7445
Celil Eren Kalkan <celilerenkalkan@hotmail.com>
2023-07-30 14:00:02 +0300

    Added Zooming

--- Sanlab Case/Assets/Scripts/CameraController.cs
+++ Sanlab Case/Assets/Scripts/CameraController.cs
@@ -5,9 +5,18 @@ public class CameraController : MonoBehaviour
     private const float Speed = 1;
     private float _xAxis;
     private float _yAxis;
+    
+    float minFov = 15f;
+    float maxFov = 90f;
+    float sensitivity = 10f;
 
     private void Update()
     {
+        float fov = Camera.main.fieldOfView;
+        fov += Input.GetAxis("Mouse ScrollWheel") * sensitivity;
+        fov = Mathf.Clamp(fov, minFov, maxFov);
+        Camera.main.fieldOfView = fov;
+        
         if (!Input.GetMouseButton(1)) return;
         
         transform.Rotate(new Vector3(Input.GetAxis("Mouse Y") * Speed, -Input.GetAxis("Mouse X") * Speed, 0));
